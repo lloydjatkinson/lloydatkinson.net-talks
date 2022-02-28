2564539052825703376c9d36273289ff64194ab8
Lloyd Atkinson <lloydjatkinson@users.noreply.github.com>
2022-02-28 23:50:34 +0000

    chore: Base path

--- build.sh
+++ build.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-cd document-proposal-management-with-finite-state-machines
+cd document-proposal-management-with-finite-state-machines --base /document-proposal-management-with-finite-state-machines/talk
 npm ci
 npm run build
 cd ..
