53c471cf848d63cffa010262333ffbb0c864d8bd
Lloyd Atkinson <lloydjatkinson@users.noreply.github.com>
2022-02-28 23:53:41 +0000

    chore: wrong argument

--- build.sh
+++ build.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-cd document-proposal-management-with-finite-state-machines --base /document-proposal-management-with-finite-state-machines/talk
+cd document-proposal-management-with-finite-state-machines
 npm ci
-npm run build
+npm run build --base /document-proposal-management-with-finite-state-machines/talk
 cd ..
