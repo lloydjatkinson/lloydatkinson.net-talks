e90dab23739f4804254fd58e6890abed424f8ae5
Lloyd Atkinson <lloydjatkinson@users.noreply.github.com>
2022-03-01 15:32:14 +0000

    chore: testing stuff out

---
+++ build-thumbnails.sh
@@ -0,0 +1,8 @@
+#!/usr/bin/env bash
+
+# TODO do this automatically
+
+cd document-proposal-management-with-finite-state-machines/dist
+mv slidev-exported.pdf document-proposal-management-with-finite-state-machines.pdf
+pdftocairo document-proposal-management-with-finite-state-machines.pdf -jpeg -singlefile -scale-to 400
+cd ..
