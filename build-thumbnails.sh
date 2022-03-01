12b88ecafd5b6577f02f4321fe51b08097b3df17
Lloyd Atkinson <lloydjatkinson@users.noreply.github.com>
2022-03-01 16:26:40 +0000

    chore: move image up one because dist is ignored

--- build-thumbnails.sh
+++ build-thumbnails.sh
@@ -5,4 +5,5 @@
 cd document-proposal-management-with-finite-state-machines/dist
 mv slidev-exported.pdf document-proposal-management-with-finite-state-machines.pdf
 pdftocairo document-proposal-management-with-finite-state-machines.pdf -jpeg -singlefile -scale-to 400
+mv document-proposal-management-with-finite-state-machines.jpg ..
 cd ..
