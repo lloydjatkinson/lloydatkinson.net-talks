0d95c0744e52d56647d3af536f373f2c69ade704
Lloyd Atkinson <lloydjatkinson@users.noreply.github.com>
2022-03-03 22:14:14 +0000

    chore: increase thumbnail resolution

--- build-thumbnails.sh
+++ build-thumbnails.sh
@@ -4,11 +4,11 @@
 
 cd document-proposal-management-with-finite-state-machines/dist
 mv slidev-exported.pdf document-proposal-management-with-finite-state-machines.pdf
-pdftocairo document-proposal-management-with-finite-state-machines.pdf -jpeg -singlefile -scale-to 400
+pdftocairo document-proposal-management-with-finite-state-machines.pdf -jpeg -singlefile -scale-to 500
 mv document-proposal-management-with-finite-state-machines.jpg ..
 cd ..
 
 cd unit-testing-and-test-driven-development
-pdftocairo unit-testing-and-test-driven-development.pdf -jpeg -singlefile -scale-to 400
+pdftocairo unit-testing-and-test-driven-development.pdf -jpeg -singlefile -scale-to 500
 mv unit-testing-and-test-driven-development.jpg ..
 cd ..
